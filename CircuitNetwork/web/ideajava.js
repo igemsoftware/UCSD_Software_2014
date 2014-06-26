178db107a34433d1e49b64bbb0257bf888ba5de6
Lauren Crudup <lauren.crudup@gmail.com>
2014-06-26 09:06:52 -0700

    Files for webpage (html, css, java) containing Assignment 1 and designs,added functions: linked page, fixed buttons

---
+++ CircuitNetwork/web/ideajava.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+    
+    $('.Home').click(function(){
+        $('#text').append('<p>Welcome to the UCSD iGEM home page<p>');
+    });
+    
+});
+
+
