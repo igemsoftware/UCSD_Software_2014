90c2eca508faef6a38a5bf45e8f1d0eb5aebc336
Kartikeya Puri <kspuri@ucsd.edu>
2014-06-29 19:23:35 -0700

    trying cookies

--- CircuitNetwork/src/java/communication/AuthenticationServlet.java
+++ CircuitNetwork/src/java/communication/AuthenticationServlet.java
@@ -8,6 +8,7 @@ package communication;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,15 +33,16 @@ public class AuthenticationServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             String input = request.getParameter("key");
+            //String input2 = request.getParameter("password");
                System.out.println(input);
-//            response.setContentType("application/json");
+//            response.setContentType("application/json");  
             String responseString = "from the server";
             out.write("{data:"+responseString+"}");
 
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -66,7 +68,41 @@ public class AuthenticationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        // Create cookies for first and last names.      
+      Cookie firstName = new Cookie("Login",
+                      request.getParameter("first_name"));
+      Cookie lastName = new Cookie("Password",
+                      request.getParameter("last_name"));
+
+      // Set expiry date after 24 Hrs for both the cookies.
+      firstName.setMaxAge(60*60*24); 
+      lastName.setMaxAge(60*60*24); 
+
+      // Add both the cookies in the response header.
+      response.addCookie( firstName );
+      response.addCookie( lastName );
+
+      // Set response content type
+      response.setContentType("text/html");
+ 
+      PrintWriter out = response.getWriter();
+      String title = "Setting Cookies Example";
+      String docType =
+      "<!doctype html public \"-//w3c//dtd html 4.0 " +
+      "transitional//en\">\n";
+      out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>Login</b>: "
+                + request.getParameter("first_name") + "\n" +
+                "  <li><b>Password</b>: "
+                + request.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
     }
 
     /**
