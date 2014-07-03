5a24b41b3712a98e4da63dfea9d49be5cbaaf03d
Kt-Er <hyeerna@ucsd.edu>
2014-07-03 13:05:31 -0700

    Made pydoc_template.py

---
+++ CircuitNetwork/src/pydoc_template.py
@@ -0,0 +1,134 @@
+'''Subtitle
+
+Descriptive paragraph 1
+
+Descriptive paragraph 2
+
+***************************************
+@author: Kwat
+@whatever 1: Whatever 1
+@whatever 2: Whatever 2
+***************************************
+
+'''
+
+# import package_1
+# import package_2
+
+global_variable_1 = 'global variable 1'
+global_variable_2 = 'global variable 2'
+
+class class_1:
+    '''Descriptive paragraph 1
+    
+    Descriptive paragraph 2
+    
+    '''
+    
+    def __init__(self, arg_1, arg_2):
+        '''Descriptive paragraph 1
+        
+        Descriptive paragraph 2
+        
+        Arguments:
+        
+        arg_1 - description 1
+        arg_2 - description 2
+        
+        '''
+        
+        self._arg_1_ = arg_1
+        self._arg_2_ = arg_2
+
+
+
+def function_1(arg_1, arg_2):
+    '''Descriptive paragraph 1
+    
+    Descriptive paragraph 2
+    
+    Arguments:
+    
+    arg_1 - description 1
+    arg_2 - description 2
+    
+    '''
+    
+    print("function_1")
+
+def function_2(arg_1, arg_2):
+    '''Descriptive paragraph 1
+    
+    Descriptive paragraph 2
+    
+    Arguments:
+    
+    arg_1 - description 1
+    arg_2 - description 2
+    
+    '''
+    
+    print("function_2")
+
+#==============================
+# Descriptive paragraph 1
+#
+# Descriptive paragraph 2
+#
+# Arguments:
+#
+# arg_1 - description 1
+# arg_2 - description 2
+#
+#==============================
+def function_3(arg_1, arg_2):
+    print("function_3")
+
+def _hidden_function_1(arg_1, arg_2):
+    '''Descriptive paragraph 1
+    
+    Descriptive paragraph 2
+    
+    Arguments:
+    
+    arg_1 - description 1
+    arg_2 - description 2
+    
+    '''
+    
+    print("_hidden_function_1")
+
+def _hidden_function_2(arg_1, arg_2):
+    '''Descriptive paragraph 1
+    
+    Descriptive paragraph 2
+    
+    Arguments:
+    
+    arg_1 - description 1
+    arg_2 - description 2
+    
+    '''
+    
+    print("_hidden_function_2")
+
+#==============================
+# Descriptive paragraph 1
+#
+# Descriptive paragraph 2
+#
+# Arguments:
+#
+# arg_1 - description 1
+# arg_2 - description 2
+#
+#==============================
+def _hidden_function_3(arg_1, arg_2):
+    print("_hidden_function_3")
+
+
+
+if __name__ == '__main__':
+    print("main")
+
+
