a6f2b9fa9edb7275f252e421672dbd0e5ec58534
luyao61 <luyao912@gmail.com>
2014-07-13 22:11:12 -0700

    2 tables created

--- CircuitNetwork/src/sql/SQLreader.py
+++ CircuitNetwork/src/sql/SQLreader.py
@@ -30,7 +30,7 @@ def SQLreader(file):
     conn = sqlite3.connect('igemDatabase.db.txt')
     c = conn.cursor()
 
-    c.execute('DROP TABLE igemDatabase')
+    c.execute('DROP TABLE IF EXISTS igemDatabase')
     # Create table
     c.execute('CREATE TABLE igemDatabase (' +  ", ".join(column) + ')')
 
@@ -39,21 +39,72 @@ def SQLreader(file):
     for i in range(1,len(table)):
         c.execute(sql.SQLInsertWrapper('igemDatabase',column,table[i][:8]))
 
-#c.execute(sql.SQLInsertWrapper("igemDatabase",column,table[2][:8]))
+
+#c.execute('SELECT * FROM igemDatabase;' )
+#print c.fetchone()
+
 
 
     # Save (commit) the changes
     conn.commit()
 
+
+    c.execute("SELECT NAME,COMPONENT,AUTHOR,ARTICLE,JOURNAL,YEAR FROM igemDatabase")
+    device = c.fetchall()
+
+    c.execute("SELECT INPUT,OUTPUT FROM igemDatabase")
+    intermediate = c.fetchall()
+
+    '''
+    for row in rows:
+        print row
+    '''
+    
+    
+    connD = sqlite3.connect('igemDBdevice.db.txt')
+    d = connD.cursor()
+    d.execute('DROP TABLE IF EXISTS igemDBdevice')
+    # Create table
+    d.execute('CREATE TABLE igemDBdevice (NAME,COMPONENT,AUTHOR,ARTICLE,JOURNAL,YEAR)')
+    d.executemany('INSERT INTO igemDBdevice VALUES (?,?,?,?,?,?)', device)
+    connD.commit()
+
+    d.execute("SELECT * FROM igemDBdevice")
+    
+    device = d.fetchall()
+    
+    for row in device:
+        print row
+
+
+    connI = sqlite3.connect('igemDBintermediate.db.txt')
+    i = connI.cursor()
+    i.execute('DROP TABLE IF EXISTS igemDBintermediate')
+    # Create table
+    i.execute('CREATE TABLE igemDBintermediate (INPUT,OUTPUT)')
+    i.executemany('INSERT INTO igemDBintermediate VALUES (?,?)', intermediate)
+    connI.commit()
+
+    i.execute("SELECT * FROM igemDBintermediate")
+    intermediate = i.fetchall()
+    for row in intermediate:
+        print row
+
+
+
+
+
+
+
+
+
+
+
+
+
     # We can also close the connection if we are done with it.
     # Just be sure any changes have been committed or they will be lost.
     conn.close()
-
-
-
-
-
-
 
 
 SQLreader('ML_June_2013.txt')
