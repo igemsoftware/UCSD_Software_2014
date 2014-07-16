a1cb96aad3b32a76c40ff2541a36f98426604a94
Gary Le <ggle@ucsd.edu>
2014-07-16 11:50:04 -0700

    cytohtml can now add imgs in separate divs, toggle the divs, and export/load cytoscape JSON using the ready function in CytoImgWip.js. All cytoscape functions are handled by the js file.

--- CircuitNetwork/web/CytoImgWip.js
+++ CircuitNetwork/web/CytoImgWip.js
@@ -9,7 +9,12 @@ $('#cy').cytoscape({
         'background-fit': 'cover',
         'border-color': '#000',
         'border-width': 3,
-        'border-opacity': 0.5}),
+        'border-opacity': 0.5})
+    .selector('.faded')
+      .css({
+        'opacity': 0.25,
+        'text-opacity': 0
+      }),
  elements:{
    nodes:[
        {data: {id: 'a', name: 'a'} },
@@ -21,14 +26,39 @@ $('#cy').cytoscape({
  },
   
  layout: {
-    name: 'breadthfirst',
+    name: 'cose',
     directed: true,
     padding: 10
   },
  ready: function(){
     window.cy = this;
-    
-    cy.on('tap', 'node', function(){
+    //Hides all img divs(secret class) on document load
+    //currently hard-coded. This piece needs editing after database is up.
+    $('.secret').hide(true);
+    //get into position
+    cy.$('#a').position({x: 100, y: 50});
+    cy.$('#b').position({x: 300,y: 50});
+    //hiding/showing div's that belong to a node
+     cy.on('tap', 'node', function(){
+        //retrieve clicked node's identity
+        //this var allows functions to be directed towards clicked node only
+        var identity = '#' + this.data('id');
+        //Unit Test. Checks that var= node id 
+        //alert(identity);
+        //retrieve node's position
+        var posit = this.position();
+        //Unit Test. Checks that node position was grabed. 
+        //alert(String(position.x + ", " + position.y));
+        //changes the location of the div's node. Doesn't work atm
+         $(identity).css({
+            'left': posit.x,
+            'top': posit.y
+        });
+        //toggles the html div & img w/ same id as clicked node
+        $(identity).toggle();
+    });
+    //defunct way of getting images by creating divs. Possibly useful later.
+    /*cy.on('click', 'node', function(){
         //adding div's on click centered at node
         var mouseX = event.pageX;
         var mouseY = event.pageY;
@@ -46,7 +76,7 @@ to everybody!</div>'
             'left': mouseX - 50,
             'background-color':'#B2B2CC'
         });
-    });
+    });*/
      
  }
 
