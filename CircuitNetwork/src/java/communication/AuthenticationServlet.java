5610081a727992bf9fa0ec8129643246568f4633
vsosnovs <valeriysosnovskiy@Valeriy-Sosnovskiys-MacBook-Pro.local>
2014-07-16 17:15:10 -0700

    cookies

--- CircuitNetwork/src/java/communication/AuthenticationServlet.java
+++ CircuitNetwork/src/java/communication/AuthenticationServlet.java
@@ -32,90 +32,99 @@ public class AuthenticationServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-            //get json data parameters sent by client
-            String user = request.getParameter("user");
-            String password = request.getParameter("password");
+        
+        //get json data parameters sent by client
+        //the user and password id's of the login page 
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+        
+        //System.out.println(user);
+        //System.out.println(password);
+        out.write("authentication processed");
+        
+        if(password.equals("welcome")){
+            out.write("Welcome to iGEM");
+            out.write("<br>Welcome," + user);
+        //create a new cookie named authenticate with value authenticated
+        Cookie authenticateCookie = new Cookie("authenticate", "authenticated");
+        //add cookie once clicked on the login 
+        response.addCookie(authenticateCookie);
+        //set the age of the cookie
+        //set the age to two hours        
+        authenticateCookie.setMaxAge(60 * 120);
+        //set to the next page
+        response.sendRedirect("Demo_Site_Login.html");
+        }
+        
+        else{
+            out.print("password or username error");
+            response.sendRedirect("Demo_Site.html");
+        }
+        
+        out.close();
 
-//            create a new cookie named authenticate with value authenticated
-            Cookie authenticateCookie = new Cookie("authenticate", "authenticated");
-//            set the age of the cookie
-            authenticateCookie.setMaxAge(60 * 60); //cookie lasts for an hour
-//            add cookie to responsej
-            response.addCookie(authenticateCookie);
-            
+        //log out portion
+        String LogOut = request.getParameter("LogOut");    
+        Cookie authenticateCookie = new Cookie("authenticate", "authenticated");
+        authenticateCookie.setMaxAge(-1);
+        response.addCookie(authenticateCookie);
+        
+        out.print("Logged out");
+        
+
     }
-    
-    
 
-    
-/*
- * Processes requests for HTTP <code>POST</code> methods.
- *
- * @param request servlet request
- * @param response servlet response
- * @throws ServletException if a servlet-specific error occurs
- * @throws IOException if an I/O error occurs
- */
-protected void processGetRequest(HttpServletRequest request, HttpServletResponse response)
+    /*
+     * Processes requests for HTTP <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processGetRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-         
 
-        
         //redirect the page to another page (can call for login button)
-//        response.sendRedirect("index.html");
+//        response.sendRedirect("Demo_Site.html");
         PrintWriter out = response.getWriter();
-      
-           
-           String line = null;
-           System.out.print("Type in your command");
-           
-           //wraps the BufferedReader around the InputStreamReader
-           //Reads text from a character-input stream, buffering characters 
-           //so as to provide for the efficient reading of characters, arrays, and lines.
-           BufferedReader readMe = new BufferedReader(new InputStreamReader(System.in));
-           try{
-                
-                //reads the line from the command line
-                line = readMe.readLine();
-               
-                /*provides methods for performing input from the process, 
-                 *performing output to the process, waiting for the process to 
-                 *complete, checking the exit status of the process, 
-                 *and destroying (killing) the process.
-                 */
-                Process process = Runtime.getRuntime().exec(line);
-            //Returns the output stream connected to the normal input of the subprocess
-            OutputStream outputStream = process.getOutputStream();
-            out.write(outputStream.toString());
-               
-                 } catch (IOException ioe) {
-         System.out.println("Error Error");
-         System.exit(1);
-            }
-            
-            //close the reader 
-            readMe.close();
-            System.out.println("Thank you ");
+        String commandString = request.getParameter("command");
         
-        
-      out.write("Good things happened");
-       
-       
+        System.out.println(commandString);
+        String result = executeCommand(commandString);
+        out.write(result);
 
-       
-       
-        
-        
-       
-}
-   
-  
+    }
+
+    private static String executeCommand(String command) {
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader
+                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "no result";
+        }
+
+        return output.toString();
+
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP
-     * <code>GET</code> method.
+     * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -123,14 +132,13 @@ protected void processGetRequest(HttpServletRequest request, HttpServletResponse
      * @throws IOException if an I/O error occurs
      */
     @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response )
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-           processGetRequest(request, response);
+        processGetRequest(request, response);
     }
 
     /**
-     * Handles the HTTP
-     * <code>POST</code> method.
+     * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -138,7 +146,7 @@ protected void processGetRequest(HttpServletRequest request, HttpServletResponse
      * @throws IOException if an I/O error occurs
      */
     @Override
-        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processPostRequest(request, response);
     }
@@ -149,9 +157,8 @@ protected void processGetRequest(HttpServletRequest request, HttpServletResponse
      * @return a String containing servlet description
      */
     @Override
-        public String getServletInfo() {
+    public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
-    
 }
--- CircuitNetwork/web/Demo_JS.js
+++ CircuitNetwork/web/Demo_JS.js
@@ -1,12 +1,33 @@
- 
+$(document).ready(function() {
 
-$(document).ready(function(){
-  
-    $('#ping').click(function(){
-    alert("about to ping");
-    $.get("AuthenticationServlet", {"command":"ping"},function(data){
-        alert(data);
+    $('#ping').click(function() {
+        var commandString = $('#command').val();
+        alert('about to execute: '+commandString);
+        var data = {user: "some user", password: "password"};
+        $.post("AuthenticationServlet", data, function(data) {
+
+            alert(data);
+        });
+    });
+    
+   $('#login').click(function(){
+      var user = $('#user').val();
+      var password =$('#password').val();
+      alert("you are logged in");
+      var data = {user: "some user", password: "password"};
+      $.post("AuthenticationServlet",data, function(data){
+          alert(data);
+          alert("whats up");
+      });
+    });
+    
+    $('#LogOut').click(function(){
+       var LogOut =$('#LogOut').val();
+       alert("about to log out");
+       var data={user: "", password:""};
+       $.post("AuthenticationServlet",data, function(data){
+          alert(data);
+          alert("back");
+      });
     });
 });
-});
-
