578b8a577de75051e60c80318215bc3c054de60f
Kartikeya Puri <kspuri@ucsd.edu>
2014-08-08 12:31:45 -0700

    cyNetShare all files

--- CircuitNetwork/web/cyjs-sample/app/scripts/services/network.js
+++ CircuitNetwork/web/cyjs-sample/app/scripts/services/network.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Created by kono on 2014/01/24.
  */
 
+'use strict';
 
+/**
+ * Services that persists and retrieves TODOs from localStorage
+ */
+angular.module('cyViewerApp')
+    .factory('Network', ['$resource', function ($resource) {
+        return $resource('/view/:networkUrl');
+    }]
+);
