4e00fa855368d28d596ccd903ba7fd115971f226
Gary Le <ggle@ucsd.edu>
2014-08-20 10:14:52 -0700

    Update interactivity to d3.js plots. Streamlining input of points and data.

--- CircuitNetwork/web/ngGraph.js
+++ CircuitNetwork/web/ngGraph.js
@@ -1,5 +1,160 @@
-var graphApp = angular.module('graphBuilder',[]);
 
-graphApp.controller('AxesCtrl',function(){
-    
+//preparing the Angular Application
+var graphApp = angular.module('graphApp',[]);
+//setting default proportonality constants for Diff EQ's
+var k = [1,1];
+var newK;
+var eqnDefault = function(i){
+        return k[0]*i + k[1];
+};
+var equation = eqnDefault;
+
+//Where we receive the equation
+var eqnNew;
+
+//defining the size of the graph.
+var m = [80,80,80,80]; //margins top,left,bottom,left
+var h = 500 - m[0] - m [2]; //height
+var w = 500 - m[1] - m[3]; //width
+
+//Variables for the points
+var domain = 100; //sample domain needs to be bound to data from Ryan's modeling
+var eqnDomain = [];//An array of the desired domain.
+var range = []; //range computed from domain.
+var points =[]; //the points used by d3.js
+var rangeMax; //maximum range
+
+//Proportionality Constant Angular Controller
+graphApp.controller('KCtrl',function($scope){
+    $scope.constants = ["k1", "k2"];
+    $scope.default = {
+        //setting prop. constants to default values
+        constants: {
+            "k1": k[0],
+            "k2": k[1]
+        }
+    };
+    $scope.collectData = function (){
+        newK = $scope.default.constants;
+        console.log(newK);
+        newLine(domain);
+    };
 });
+
+//Creating the d3.js controlled SVG graph.
+//creating an array for the listed domain.
+var domainSet = function(time){
+    for(i = 0; i <= time; i++){
+        eqnDomain.push(i);
+    };
+};
+//hard coding a sample domain.
+domainSet(domain);
+
+var rangeSet = function(time) {
+    for (i = 0; i <= d3.max(time); i++){
+        var y = equation(i);
+        range.push(y);
+    };
+};
+//Preparing y-component of points
+rangeSet(eqnDomain);
+rangeMax = d3.max(range);
+
+//Setting eqnDomain and range to points
+var pointsSet = function(input,output){
+    for (i = 0; i <= d3.max(input); i++){
+        //Setting up entry into the points array.
+        var point = [];
+        //Assigning x-value of point "i"
+        point.push(input[i]);
+        //Assigning y-value of point "i"
+        point.push(output[i]);
+        //Adding point to array of points.
+        points.push(point);
+    };
+};
+//Setting points.
+pointsSet(eqnDomain,range);
+
+//setting up the function for the line.
+var line = d3.svg.line()
+        .x(function(d) { 
+            //console.log("plotting x-value:" + d[0]);
+            return xScale(d[0]); 
+        })
+        .y(function(d){
+            //console.log("plotting x-value:" + d[1])
+           return yScale(d[1]);
+        });
+
+//determing the scale of the axes lines.
+var xScale = d3.scale.linear().domain([0,domain]).range([0,w]);
+var yScale = d3.scale.linear().domain([0,domain]).range([h,0]);
+
+function make_x_axis() {        
+    return d3.svg.axis()
+        .scale(xScale)
+         .orient("bottom")
+         .ticks(10);
+}
+
+function make_y_axis() {        
+    return d3.svg.axis()
+        .scale(yScale)
+        .orient("left")
+        .ticks(10);
+}
+//create the SVG containter
+var plot = d3.select("#graph").append("svg:svg")
+        .attr("height", h + m[0] +m[2])
+        .attr("width", w + m[1] + m[3])
+    .append("svg:g")
+        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+    //adding x-grid
+    plot.append("g")         
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + h + ")")
+        .call(make_x_axis()
+            .tickSize(-h, 0, 0)
+            .tickFormat("")
+        );
+
+    //adding y-grid
+    plot.append("g")         
+        .attr("class", "grid")
+        .call(make_y_axis()
+            .tickSize(-w, 0, 0)
+            .tickFormat("")
+        );
+
+    //create left y-axis
+    var yAxisMain = d3.svg.axis().scale(yScale).ticks(10).orient("left");
+    // Add the main y-axis
+    plot.append("svg:g")
+            .attr("class", "main axis")
+            //.attr("transform", "translate(0," + h + ")")
+            .call(yAxisMain);
+
+    //create bottom x-axis
+    var xAxisMain = d3.svg.axis().scale(xScale);
+    // Add the main x-axis
+    plot.append("svg:g")
+            .attr("class", "main axis")
+            .attr("transform", "translate(0," + h + ")")
+            .call(xAxisMain);
+    
+    // Adding the line to the graph.
+    plot.append("svg:path").attr("d", line(points));
+
+var newLine = function(newDomain){
+    eqnNew = function(i){
+        return newK.k1*i+newK.k2;
+    };
+    var equation = eqnNew;
+    domainSet(newDomain);
+    rangeSet(eqnDomain);
+    pointsSet(eqnDomain,range);
+    plot.append("svg:path").attr("d", line(points));
+};
