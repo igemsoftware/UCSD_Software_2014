08bdd9a2fdb83b2313d287bfe221567694435b65
Kt-Er <hyeerna@ucsd.edu>
2014-09-12 01:17:12 -0700

    updated sql scripts:

--- kwat_scripts/database_pytools.py
+++ kwat_scripts/database_pytools.py
@@ -11,58 +11,71 @@ import pandas as pd
 
 #def database_open(database = "SBiDer_v1.db"):
 def open(database):
+    """Opens the specified database by making a connection to it."""
     global conn 
     conn = sqlite3.connect(database)
     conn.text_factory = str
     global sql 
     sql = conn.cursor()
     
-def print_table(table_name):
-    cur = custom("SELECT * FROM " + table_name)
-    table_list = cur.fetchall()
-    for row in table_list:
-        print row
+def close():
+    """Closes the mostly recently opened database."""
+    conn.commit()
+    sql.close()
     
-def make_db(database):
-    #Setting up all of the csv names to extract the data
-    info_names = ["plasmid", "operon", "part", "interactor", "input", "output", "opr", "optr", "oitr", "ootr"]
-
-    #Creating tables of genetic constructs and interactors
-    plasmid = 'CREATE TABLE plasmid (id VARCHAR(50), name VARCHAR(50), miriam_id VARCHAR(50), title VARCHAR(50), authors VARCHAR(50), journal VARCHAR(50), year VARCHAR(50));'
-    operon = 'CREATE TABLE operon (id VARCHAR(50), name VARCHAR(50), sbol VARCHAR(50));'
-    part = 'CREATE TABLE part (id VARCHAR(50), name VARCHAR(50));'
-    interactors = 'CREATE TABLE interactor (id VARCHAR(50),name VARCHAR(50), type VARCHAR(50));'
-    #Creating tables of transitions that are used for the petri nets
-    inputt = 'CREATE TABLE input (id VARCHAR(50), interactor_id VARCHAR(50), repressor bool);'
-    output = 'CREATE TABLE output (id VARCHAR(50), interactor_id VARCHAR(50));'    
-    #Creating tables of relationships
-    opr = 'CREATE TABLE opr (id VARCHAR(50), operon_id VARCHAR(50), plasmid_id VARCHAR(50), direction VARCHAR(50), Main NUM(10));'
-    optr = 'CREATE TABLE optr (id VARCHAR(50), operon_id VARCHAR(50), part_id VARCHAR(50), position NUM(10));'
-    #can use input.txt
-    oitr = 'CREATE TABLE oitr (id VARCHAR(50), operon_id VARCHAR(50), interactor_id VARCHAR(50));'
-    #can use output.txt
-    ootr = 'CREATE TABLE ootr (id VARCHAR(50), operon_id VARCHAR(50), interactor_id VARCHAR(50));'
+def make_db():
+    """Recreation of the sbider database tables. Careful! You will override the data"""
+    print "CAUTION: you will override the data!"
+    print "Do you really want to proceed? Y or N"
+    user_caution = raw_input()
+    if user_caution == 'Y':
+        info_names = ["plasmid", "operon", "part", "interactor", "input", "output",
+            "opr", "optr", "oitr", "ootr"]
+            
+        plasmid = '''CREATE TABLE plasmid (id VARCHAR(50), name VARCHAR(50), 
+            miriam_id VARCHAR(50), title VARCHAR(50), authors VARCHAR(50), 
+            journal VARCHAR(50), year VARCHAR(50));'''
+        operon = '''CREATE TABLE operon (id VARCHAR(50), name VARCHAR(50), 
+            sbol VARCHAR(50));'''
+        part = 'CREATE TABLE part (id VARCHAR(50), name VARCHAR(50));'
+        interactors = '''CREATE TABLE interactor (id VARCHAR(50),name VARCHAR(50), 
+            type VARCHAR(50));'''
+        inputt = '''CREATE TABLE input (id VARCHAR(50), interactor_id VARCHAR(50),
+            repressor bool);'''
+        output = '''CREATE TABLE output (id VARCHAR(50), 
+            interactor_id VARCHAR(50));'''    
+        opr = '''CREATE TABLE opr (id VARCHAR(50), operon_id VARCHAR(50), 
+            plasmid_id VARCHAR(50), direction VARCHAR(50), Main NUM(10));'''
+        optr = '''CREATE TABLE optr (id VARCHAR(50), operon_id VARCHAR(50), 
+            part_id VARCHAR(50), position NUM(10));'''
+        oitr = '''CREATE TABLE oitr (id VARCHAR(50), operon_id VARCHAR(50), 
+            interactor_id VARCHAR(50));'''
+        ootr = '''CREATE TABLE ootr (id VARCHAR(50), operon_id VARCHAR(50), 
+            interactor_id VARCHAR(50));'''
+        login = '''CREATE TABLE login (id VARCHAR(50), email VARCHAR(50), 
+            password VARCHAR(50));'''
+        
+        table_list =[plasmid, operon, part, interactors, opr, optr, oitr, ootr, inputt, output]
+        for table in table_list:
+            custom(table)
+    else:
+        print "You either typed 'N' or did not use the specified characters"
+        
+def insert(table_name,cols,new_row):
+    """Inserts data into the given database table
+    Args:
+        table_name, that table that you wish to insert into
+        cols, the columns that you want to insert into
+        new_row, the values that correspond to the columns
     
+    Examples:
+        ex 1. Inserting into plasmid table and filling in all the columns. 
+    """
+    command = sqlpy.sql_insert(table_name, cols,new_row)
+    print "\n" * 3
+    print command
+    sql.execute(command)
     
-    #care. If you actually do this then you WILL OVERRIDE the current database. 
-    table_list =[plasmid, operon, part, interactors, opr, optr, oitr, ootr, inputt, output]
-    for table in table_list:
-        custom(table)
-
-#Declaring list global variables for integration with accession functions
-
-plasmid_table = ["ID", "Name", "Miriam_ID"]
-operon_table = ["ID", "Name", "Image_Path"]
-part_table = ["ID", "Name"]
-interactor_table = ["ID", "Name","Repressor"]
-opr_table = ["ID", "Operon_ID", "Plasmid_ID"]
-optr_table = ["ID", "Operon_ID", "Part_ID", "Position"]
-oitr_table = ["ID", "Operon_ID", "Input_ID"]
-ootr_table = ["ID", "Operon_ID", "Output_ID"]
-input_trans_table = ["ID", "Input", "Repressor"]
-output_trans_table = ["ID", "Output"]
-    
-#--------------------DB Accessions--------------------#                
 def select(table_name, col_names = ["ID"], w_col = None, w_opt = None,
     w_var = None,w_bool = None, group = False, h_col = None, h_bool = None, 
     h_value = None):
@@ -103,48 +116,40 @@ def select(table_name, col_names = ["ID"], w_col = None, w_opt = None,
     sql.execute(command)
     return sql.fetchall()
     
-def insert(table_name,cols,new_row):
-    """Inserts data into the given database table
-    Args:
-        table_name, that table that you wish to insert into
-        cols, the columns that you want to insert into
-        new_row, the values that correspond to the columns
-    
-    Examples:
-        ex 1. 
-    """
-    command = sqlpy.sql_insert(table_name, cols,new_row)
-    print "\n" * 3
-    print command
-    sql.execute(command)
-    
 #def update():
     
 def custom(command):
     """Execute a custom made sql command
-    Return:
+    Return: A cursor object. If the sql command used produces a result such as
+    the select command then this cursor object must be assigned to a 
+    variable and the commmand fetchone() or fetchall() should be used. 
+    
+    Ex 1. 
+    cur = cu 
     """
     return sql.execute(command)
-    
-def close():
-    conn.commit()
-    sql.close()
-    
+        
+def print_table(table_name):
+    """Prints out the specified table for visualization purposes only."""
+    cur = custom("SELECT * FROM " + table_name)
+    table_list = cur.fetchall()
+    for row in table_list:
+        print row               
     
 #--------------------Logic Accessions--------------------#        
 def single_trans_to_bool(trans_ID):
-    """
-    Determine the boolean logic of a single transition using the ID
-    @param trans_ID, the transition for which logic is desired
+    """Determine the boolean logic of a single transition using the ID
+    Args:
+        trans_ID: the transition for which logic is desired
     """
     trans_list = sql.execute('''SELECT Interactor_ID, NOT/
         FROM input_trans WHERE Input_Transition_ID = ''' + trans_ID)
     return bool_trans(trans_list)
 
 def single_operon_to_bool(operon_ID):
-    """
-    Determine the boolean logic of a single operon using the ID
-    @param operon_ID, the operon for which logic is desired
+    """Determine the boolean logic of a single operon using the ID
+    Args:
+        operon_ID: the operon for which logic is desired
     """
     operon_list = sql.execute('''SELECT Input_Transition_ID from oitr 
         where Operon_ID = ''' + operon_ID)
@@ -156,11 +161,12 @@ def single_operon_to_bool(operon_ID):
         return ' OR '.join(operon_list)    
 
 def to_trans_logic():
-    """
-    Determine the boolean logic for all transitions in the database
-    Collecting all of the transition-interactor information inside of a dictionary 
+    """Determine the boolean logic for all transitions in the database
+    Collecting all of the transition-interactor information inside 
+    of a dictionary 
         #Key: trans_ID
-        #Value: List of [[interactor1, not_boolean1], [interactor2, not_boolean2], ...]
+        #Value: List of [[interactor1, not_boolean1], 
+            [interactor2, not_boolean2], ...]
     """  
     trans_list = sql.execute("SELECT Interactor_ID, Input_Transition_ID, NOT")
     trans_dict = {}
@@ -212,20 +218,20 @@ def to_bool_operon():
     
 #--------------------helpers--------------------#        
 def bool_trans(interactor): 
-    """
-    Determine the boolean logic of transition based on interactor
-    @param interactor, list of paired lists with interactor ID and NOT boolean
-    SUGGESTIONS: will need to convert from the interactor_ID to the 
-    interactor_name at some point. Will have to query sql to do so.
-    I think it's better to query than to make a whole new dictionary
-    with key: interactor_ID value: interactor_name
+    """Determine the boolean logic of transition based on interactor
+    
+    Args:
+        interactor: list of paired lists with interactor ID and NOT boolean
+        
+    SUGGESTIONS:
+        convert from the interactor_ID to the interactor_name 
     """
     #single input, buffer transitions 
     if len(interactor) == 1 and interactor[0][1] != False:
-        return "IF", interactor[0][0] #interactor[0][0] ID maybe also return the i_ID (or only it) and may not actually return
+        return "IF", interactor[0][0] 
     #single input, not transition
     elif len(interactor) == 1 and interactor[0][1] == True:
-        return "NOT", interactor[0][0] #interactor[0][0] maybe also return the i_ID
+        return "NOT", interactor[0][0] 
     #multiple input, and transition
     elif len(interactor) > 1:
         bool_string = ""
@@ -245,9 +251,7 @@ def bool_trans(interactor):
         return bool_string
         
 def pull_columns(col_nums):
-    """
-    Return a list of column names
-    """
+    """Returns a list of column names based on the index location"""
     columns= []
     for nums in col_nums:
         try:
@@ -258,32 +262,82 @@ def pull_columns(col_nums):
     return columns
     
 def unlist_values(to_list):
-    """
-    Stringify values in a list that are within a list
-    @param to_list, a list with lists of single values
+    """Stringify values in a list that are within a list
+    ARGS:
+        to_list, a list with lists of single values
     """
     return [''.join(x) for x in to_list]
     
 #--------------------Accessions,working--------------------#        
-def to_json():
+def to_json_node():
     """
     Obtain nodes and edge information and parse into json
     """
     nodes_table = ["interactor", "input", "output", "operon"]
     edges_table = ["itr","",""]
     
-    nodes_list = []
-    sql.execute("SELECT blah_columns from interactor" )
-    nodes_list.append(sql.fetchall())
-    sql.execute("SELECT blah_columns from input" )
-    nodes_list.append(sql.fetchall())
-    sql.execute("SELECT blah_columns from output" )
-    nodes_list.append(sql.fetchall())
-    sql.execute("SELECT blah_columns from operon" )
-    nodes_list.append(sql.fetchall())
+    interactor_list = []
+    sql.execute("SELECT id, name from interactor" )
+    interactor_list.extend(sql.fetchall())
+    
+    input_list = []
+    sql.execute("SELECT id, name from input" )
+    input_list.extend(sql.fetchall())
+    
+    output_list = []
+    sql.execute("SELECT id, name from output" )
+    output_list.extend(sql.fetchall())
+    
+    operon_list = []
+    sql.execute("SELECT id, name from operon" )
+    operon_list.extend(sql.fetchall())
+    
+    
+    nodes_list = [interactor_list, input_list, output_list, operon_list]
+    for ls in nodes_list:
+        for node in interactor_list:
+                json_str += tojson(node)
     
     f#or table in nodes_table:
         
+        
+def to_network():
+    """Making two dictionaries for the traversal.
+    Returns:
+       A tuple with two dictionaries. The first   
+    """
+    input_trans_dict = {}
+    inputt = sql.execute("SELECT id, interactor_id from input")
+    cur = inputt.fetchone()
+    count = 1
+    while isinstance(cur, tuple) is True:
+        key = cur[0]
+        if key in input_trans_dict:
+            input_trans_dict[key].add(cur[1])
+        else:
+            input_trans_dict[key] = set([cur[1]])
+        count += 1
+        cur = inputt.fetchone()
+        '''
+        key: transition ID
+        value: set with the inputs species. ex set(['a','b','c'])
+        '''
+    output_trans_dict = {}
+    output = sql.execute("SELECT id, interactor_id from output")
+    cur = output.fetchone()
+    while isinstance(cur, tuple) is True:
+        key = cur[0]
+        if key in output_trans_dict:
+            output_trans_dict[key].add(cur[1])
+        else:
+            output_trans_dict[key] = set([cur[1]])
+        cur = output.fetchone()
+    
+    return input_trans_dict, output_trans_dict
+    '''
+    key: transition ID
+    value: list with all the species produce by that operon/transition
+    '''
 
 def to_pigeon():
     """
